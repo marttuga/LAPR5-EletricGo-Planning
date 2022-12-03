da0d638c5f53879a579725f5921931c3fbb530b7
Marta Portugal <1201845@isep.ipp.pt>
2022-12-03 18:24:54 +0000

    us4 almost finished

--- Planning/Sprint B/US4-Heuristics.pl
+++ Planning/Sprint B/US4-Heuristics.pl
@@ -324,33 +324,38 @@ idArmazem('Vila do Conde',16).
 idArmazem('Vila Nova de Gaia',17).
 
 
-/*cidadeArmazem(<id>).*/
-cidadeArmazem(5).
 
-/*
-Recebendo os dados das entregas a fazer por 1 camião e dos troços entre armazéns: 
-gerar todas as trajetórias possíveis através de sequências de armazéns onde deverão ser feitas as entregas
+/* Começamos por procurar todos os armazens cujas deliveries foram feitas na data especificada
+L - Lista de armazéns
+WId - ID do armazém
 */
+warehouseRoute(Date,L):-findall(WId, entrega(_,Date,_,WId,_,_),L). 
 
-/* Todos os armazens, que guarda numa lista (L) que procura (findall) pelo WId do armazém correspondente nas entregas
-a fazer numa determinada data */
-warehouseRoute(L,Date):-findall(WId, delivery(_,Date,_,WId,_,_),L). 
+/* Retorna todas as permutações possíveis entre os armazéns cujas entregas se realizam nessa data 
+LR - Lista de rotas
+LW - Lista de armazéns
+*/
+routes(Date,LR):- warehouseRoute(Date,LW), findall(Route, permutation(LW, Route), LR).
 
-/* Juntar o armazém de Matosinhos sempre como armazém inicial e final */
-addMainWarehouse(IW, FW):-cidadeArmazem(WId), append([WId|IW],[WId],FW).
-
-/* As trajetórias possiveis, com permutação de uma lista de routes */
-routes(LR,Date):- warehouseRoute(LW,Date), findall(Route, permutation(LW, Route), LR).
-
-/* Route tendo em conta os troços e com Matosinhos que adiciona no inicio e no final */
+/* Retorna todas a permutações possiveis mas começando e acabando em Matosinhos 
+V - Primeira rota da lista de rotas
+R - Primeira rota completa
+*/
 fullRoute([],[]).
-fullRoute([V|LR], [R|FW]):- addMainWarehouse(V, R),write(R),nl, fullRoute(LR, FW).
+fullRoute([V|LR], [R|FW]):- idArmazem('Matosinhos',WId), append([WId|V],[WId],R), fullRoute(LR, FW).
 
-/* Todas as routes possíveis para uma certa data, tendo em conta o armazém de Matosinhos*/
-finalRoute(FW,Date):-routes(LR,Date), fullRoute(LR, FW).
+/* Retorna a lista final com todas as routes possíveis
+FWL- Lista final de armazéns
+LR- List de rotas
+*/
+finalRoute(Date,FWL):-routes(Date,LR), fullRoute(LR, FWL),write(FWL).
 
 
-/* 
+
+
+/*US 2
+
+
 Avaliar essas trajetórias de acordo com o tempo para completar todas as entregas e 
 voltar ao armazém base de Matosinhos e escolher a solução que permite a volta com o camião mais cedo 
 */
@@ -376,7 +381,7 @@ getCapacityWLoadAndTare(Truck,Capacity):-carateristicasCam(Truck,Tare,CapacityLo
 transporta, saber a sua tara, o peso total do Truck já cheio, ter em conta também a bateria com que se encontra e quanto demora para 
 a carregar, para no final, calcular o tempo da viagem total  */
 determineTime(Date, Truck, LW, Time):- getLoadTruck(Date, LW, LL,_), addTareTruck(Truck,LL, LLT),
-                                           cidadeArmazem(Id), append([Id|LW],[Id],Routes),
+                                            idArmazem('Matosinhos',WId), append([WId|LW],[WId],Routes),
                                            getCapacityWLoadAndTare(Truck,Capacity),
                                            carateristicasCam(Truck,_,_,BatteryLoad,_,ChargingTime),
                                            routeTime(Routes, LLT,Capacity,BatteryLoad,BatteryLoad,ChargingTime,Date,Time),!.
@@ -402,7 +407,7 @@ routeTime([W1,W2,W3|Routes], [Load1,Load2|LLT],Capacity, BatteryLoad,MaxLoad,Cha
                     entrega(_,Date,_,W2,_,DischaringTime),
                     dadosCam_t_e_ta(_, W2, W3, _,NextMaxWastedEnergy,_), 
                     NextNecessaryEnergy is NextMaxWastedEnergy * Load2 / Capacity,
-                    cidadeArmazem(Matosinhos), ((W3 == Matosinhos, BatteryEnergyArrivalWarehouse - NextNecessaryEnergy < (MaxLoad*0.2), 
+                     idArmazem('Matosinhos',WId), ((W3 == WId, BatteryEnergyArrivalWarehouse - NextNecessaryEnergy < (MaxLoad*0.2), 
                     ChargingQuantity is ((MaxLoad*0.2) - (BatteryEnergyArrivalWarehouse - NextNecessaryEnergy) ), 
                     TimeCharging is ChargingQuantity*ChargingTime/(MaxLoad*0.6),
                     NextBatteryLoad is ChargingQuantity+BatteryEnergyArrivalWarehouse,!);
@@ -433,32 +438,35 @@ update(FLPerm,Time):-
         assertz(lessTime(FLPerm,Time)));true).
 
 
+
+
+
+
  
-/*User Story 4
+/*US 4*/
 
+deleteWarehouseVisited([W|RemainingWarehouses],NextWarehouse,MissingWarehouses):- delete([W|RemainingWarehouses],NextWarehouse,MissingWarehouses).
 
-##### HEURISTICA 1- MAIS PROXIMO TEMPO OU DISTANCIA
+/*HEURISTICA 1- MAIS PROXIMO TEMPO OU DISTANCIA
 
 #retorna o armazem mais proximo. Recebe o armazem de origem, a lista de todos os armazens e o tempo. Vai entao procurar o camiao que tem o mesmo armazem de origem, o de destino e o mesmo tempo.
 #calcula o menor tempo entre os armazens, relativamente ao de origem e o que tiver menor fica o armazem escolhido mais proximo*/
 
-NearestWarehouse(_,[],1000000,_):-!.
-NearestWarehouse(Departure,[W1|Warehouses],LessTime,Warehouse):-NearestWarehouse(Departure,Warehouses,LessTime1,Warehouse1),
-                                                                dadosCam_t_e_ta(_,Departure,W1,Time,_,_),
-                                                                ((Time<LessTime1,!, LessTime is Time, Warehouse = W1);LessTime is LessTime1, Warehouse = Warehouse1).
+nearestWarehouse(_, [], 0,_):-!.
+nearestWarehouse(Departure,[W1|Warehouses],LessTime,Warehouse):-nearestWarehouse(Departure,Warehouses,LessTime1,Warehouse1), dadosCam_t_e_ta(_,Departure,W1,Time,_,_),
+                                                        ((Time<LessTime1,!, LessTime is Time, Warehouse = W1);LessTime is LessTime1, Warehouse = Warehouse1).
 
 
 
-/*# pior caso tempo-0(b^m) pior caso espaco-0(b^m)
+/*
 (bfs)-best first search
 #mais rapido e eficiente q o breadth
 # recebe o armazem de origem, lista de armazens e retorna a lista com os troços entre os armazens mais proximos
 #a partir do armazem,cidade origem e da lista vai buscar o armazem mais proximo da origem, de seguida apaga esse ja visto da lista dos armazens e manda o armazem mais proximo da origem encontrado como origem para o bfs*/
 
 bfsNearestWarehouse(_,[],[]):-!.
-bfsNearestWarehouse(Departure,[W|RemainingWarehouses],[NextWarehouse|Route]):-NearestWarehouse(Departure,[W|RemainingWarehouses],_,NextWarehouse),
-                                                                              delete([W|RemainingWarehouses],NextWarehouse,MissingWarehouses),
-                                                                              bfsNearestWarehouse(NextWarehouse,MissingWarehouses,Route).
+bfsNearestWarehouse(Departure,[W|RemainingWarehouses],[NextWarehouse|Route]):-nearestWarehouse(Departure,[W|RemainingWarehouses],_,NextWarehouse),deleteWarehouseVisited([W|RemainingWarehouses],
+                                                                                NextWarehouse,MissingWarehouses), bfsNearestWarehouse(NextWarehouse,MissingWarehouses,Route).
 
 
 
@@ -466,10 +474,8 @@ bfsNearestWarehouse(Departure,[W|RemainingWarehouses],[NextWarehouse|Route]):-Ne
 # vai entao começar por arranjar todos os armazens (onde devam ser feitas entregas naquela Date), numa lista, de seguida arranja o armazem,cidade origem, que vai ser utilizado no bfs para obter a lista de viagens 
 # finalmente a partir da Date , camiao e viagens, vai determinar os tempos para poder comparar e descobrir o melhor*/
 
-bestRouteNearestWarehouse(Date,Truck,Route,Time):-RouteWarehouses(VisitWarehouse,Date),
-                                                             cidadeArmazem(Departure),
-                                                             bfsNearestWarehouse(Departure,VisitWarehouse,Route),
-                                                             determineTime(Date,Truck,Route,Time), !.
+bestRouteNearestWarehouse(Date,Truck,Route,Time):-warehouseRoute(Date,VisitWarehouse),idArmazem('Matosinhos',WId),bfsNearestWarehouse(WId,VisitWarehouse,Route),
+                                               determineTime(Date,Truck,Route,Time), ! , format('Fastest Time rounded: = ~3f~n min', [Time]).
 
 
 
@@ -479,9 +485,8 @@ bestRouteNearestWarehouse(Date,Truck,Route,Time):-RouteWarehouses(VisitWarehouse
 # recebe a lista de armazens, Date e a massa para a posterior comparaçao 
 # começa por receber os dados e compará-los com os da entrega, neste caso comparar a GreaterMass1 com a massa da entrega respetiva naquela Date e para o armazem fornecido W1*/
 
-deliveryPlusMass([],_,0,_):-!.
-deliveryPlusMass([W1|Warehouses],Date,GreaterMass,Warehouse):-deliveryPlusMass(Warehouses,Date,GreaterMass1,Warehouse1),
-                                                           entrega(_,Date,Mass,W1,_,_),
+deliveryPlusMass([],_, 0,_):-!.
+deliveryPlusMass([W1|Warehouses],Date,GreaterMass,Warehouse):-deliveryPlusMass(Warehouses,Date,GreaterMass1,Warehouse1),entrega(_,Date,Mass,W1,_,_),
                                                            ((Mass>GreaterMass1,!, GreaterMass is Mass, Warehouse = W1);GreaterMass is GreaterMass1, Warehouse = Warehouse1),!.
 
 
@@ -489,34 +494,32 @@ deliveryPlusMass([W1|Warehouses],Date,GreaterMass,Warehouse):-deliveryPlusMass(W
 #a partir da Date e da lista vai buscar a entrega com maior massa para esse armazem, de seguida apaga esse ja visto da lista dos armazens e manda a Date e os armazens seguintes para o bfs*/
 
 bfsWarehousePlusMass(_,[],[]):-!.
-bfsWarehousePlusMass(Date,[W|RemainingWarehouses],[NextWarehouse|Route]):-deliveryPlusMass([W|RemainingWarehouses],Date,_,NextWarehouse),
-                                                                              delete([W|RemainingWarehouses],NextWarehouse,MissingWarehouses),
-                                                                              bfsWarehousePlusMass(Date, MissingWarehouses,Route).
+bfsWarehousePlusMass(Date,[W|RemainingWarehouses],[NextWarehouse|Route]):-deliveryPlusMass([W|RemainingWarehouses],Date,_,NextWarehouse),deleteWarehouseVisited([W|RemainingWarehouses],
+                                                                        NextWarehouse,MissingWarehouses), bfsWarehousePlusMass(Date, MissingWarehouses,Route).
 
 
 /*#tendo em conta a Date, o camiao, a lista dos troços de armazens(as viagens) e o tempo, obter a melhor viagem
 # vai entao começar por arranjar todos os armazens (onde devam ser feitas entregas naquela Date), numa lista, de seguida vai mandar essa lista e a Date para o bfs e vai obter a lista de viagens 
 # finalmente a partir da Date , camiao e viagens, vai determinar os tempos para poder comparar e descobrir o melhor*/
 
-bestRoutePlusMass(Date,Truck,Route,Time):-RouteWarehouses(VisitWarehouse,Date),
-                                                             bfsWarehousePlusMass(Date,VisitWarehouse,Route),
-                                                             determineTime(Date,Truck,Route,Time), !.
+bestRoutePlusMass(Date,Truck,Route,Time):-warehouseRoute(Date,VisitWarehouse),bfsWarehousePlusMass(Date,VisitWarehouse,Route),
+                                        determineTime(Date,Truck,Route,Time), !, format('Fastest Time rounded: = ~3f~n min', [Time]).
 
 
 
 
 
-/*##### HEURISTICA 3- COMBINAR TEMPO COM W MASSA 
+/*##### HEURISTICA 3- COMBINAR TEMPO COM MASSA 
 
 # obter o armazem cuja entrega tem melhor relacao entre o tempo e a massa 
 # recebe o armazem,cidade origem, a lista dos armazens, a Date e a relaçao(para posterior comparaçao),
 # começa entao por receber os dados e vai buscar o tempo do camiao (cujo armazem de origem e destino sao os mesmos que os recebidos na funçao) e vai buscar a massa da entrega( cuja Date e armazem de destino sao os mesmos recebidos na funçao e do camiao)
-# faz-se a comparaçao entre a relaçao massa da entrega tempo camiao e a relacao dos valores iniciais para obter o armazem */
+# faz-se a comparaçao entre a relaçao massa da entrega tempo camiao e a relacao dos valores iniciais para obter o armazem 
+massa/tempo -> melhor relacao é comn maior massa chega mais rapido
+*/
 
-bestRelationDelivery(_,[],_, -10000000,_):-!.
-bestRelationDelivery(Departure,[W1|Warehouses],Date,BestRelation,Warehouse):-bestRelationDelivery(Departure,Warehouses,Date,BestRelation1,Warehouse1),
-                                                           dadosCam_t_e_ta(_,Departure,W1,Time,_,_),
-                                                           entrega(_,Date,Mass,W1,_,_),
+bestRelationDelivery(_,[],_, 0,_):-!.
+bestRelationDelivery(Departure,[W1|Warehouses],Date,BestRelation,Warehouse):-bestRelationDelivery(Departure,Warehouses,Date,BestRelation1,Warehouse1), dadosCam_t_e_ta(_,Departure,W1,Time,_,_),entrega(_,Date,Mass,W1,_,_),
                                                            (((Mass/Time)>BestRelation1,!, BestRelation is (Mass/Time), Warehouse = W1);BestRelation is BestRelation1, Warehouse = Warehouse1),!.
 
 
@@ -524,16 +527,13 @@ bestRelationDelivery(Departure,[W1|Warehouses],Date,BestRelation,Warehouse):-bes
 #a partir do armazem,cidade origem , da lista e da Date vai buscar a entrega com maior massa para esse armazem, de seguida apaga esse ja visto da lista dos armazens e manda a Date e os armazens seguintes para o bfs*/
 
 bfsBestRelation(_,_,[],[]):-!.
-bfsBestRelation(Date,Departure,[W|RemainingWarehouses],[NextWarehouse|Route]):-bestRelationDelivery(Departure,[W|RemainingWarehouses],Date,_,NextWarehouse),
-                                                                              delete([W|RemainingWarehouses],NextWarehouse,MissingWarehouses),
-                                                                              bfsBestRelation(Date,NextWarehouse,MissingWarehouses,Route).
+bfsBestRelation(Date,Departure,[W|RemainingWarehouses],[NextWarehouse|Route]):-bestRelationDelivery(Departure,[W|RemainingWarehouses],Date,_,NextWarehouse),deleteWarehouseVisited([W|RemainingWarehouses],
+                                                                                NextWarehouse,MissingWarehouses),bfsBestRelation(Date,NextWarehouse,MissingWarehouses,Route).
 
 
 /*#tendo em conta a Date, o camiao, a lista dos troços de armazens(as viagens) e o tempo, obter a melhor viagem com a melhor relacao tempo e massa
 # vai entao começar por arranjar todos os armazens (onde devam ser feitas entregas naquela Date), numa lista, de seguida vai buscar a cidade/armazem origem e vai mandar a origem, a lista e a Date para o bfs e vai obter a lista de viagens 
 # finalmente a partir da Date , camiao e viagens, vai determinar os tempos para poder comparar e descobrir o melhor*/
 
-bestRouteBestRelation(Date,Truck,Route,Time):-RouteWarehouses(VisitWarehouse,Date),
-                                                        cidadeArmazem(Departure),
-                                                        bfsBestRelation(Date,Departure,VisitWarehouse,Route),
-                                                        determineTime(Date,Truck,Route,Time), !.
+bestRouteBestRelation(Date,Truck,Route,Time):-warehouseRoute(Date,VisitWarehouse),idArmazem('Matosinhos',WId),bfsBestRelation(Date,WId,VisitWarehouse,Route),
+                                             determineTime(Date,Truck,Route,Time), !, format('Fastest Time rounded: = ~3f~n min', [Time]).
