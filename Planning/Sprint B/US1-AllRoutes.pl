a3ed1659dc1fd98e7c52e035d6ce9000f922aadd
Marta Portugal <1201845@isep.ipp.pt>
2022-12-03 14:57:10 +0000

    delivery to entrega

--- Planning/Sprint B/US1-AllRoutes.pl
+++ Planning/Sprint B/US1-AllRoutes.pl
@@ -293,15 +293,14 @@ dadosCam_t_e_ta(eTruck01,17,15,53,18,0).
 dadosCam_t_e_ta(eTruck01,17,16,67,25,0). 
 
 
+
 /*deliveries
-delivery(<idEntrega>,<data>,<massaEntrefa>,<armazemEntrega>,<tempoColoc>,<tempoRet>)*/
-delivery(4439, 20221205, 200, 1, 8, 10).
-delivery(4438, 20221205, 150, 9, 7, 9).
-delivery(4445, 20221205, 100, 3, 5, 7).
-delivery(4443, 20221205, 120, 8, 6, 8).
-delivery(4449, 20221205, 300, 11, 15, 20).
-
-
+entrega(<idEntrega>,<data>,<massaEntrefa>,<armazemEntrega>,<tempoColoc>,<tempoRet>)*/
+entrega(4439, 20221205, 200, 1, 8, 10).
+entrega(4438, 20221205, 150, 9, 7, 9).
+entrega(4445, 20221205, 100, 3, 5, 7).
+entrega(4443, 20221205, 120, 8, 6, 8).
+entrega(4449, 20221205, 300, 11, 15, 20).
 
 /*id_warehouse
 idArmazem(<local>,<codigo>)*/
@@ -334,7 +333,7 @@ gerar todas as trajetórias possíveis através de sequências de armazéns onde
 L - Lista de armazéns
 WId - ID do armazém
 */
-warehouseRoute(Date,L):-findall(WId, delivery(_,Date,_,WId,_,_),L). 
+warehouseRoute(Date,L):-findall(WId, entrega(_,Date,_,WId,_,_),L). 
 
 /* Retorna todas as permutações possíveis entre os armazéns cujas entregas se realizam nessa data 
 LR - Lista de rotas
