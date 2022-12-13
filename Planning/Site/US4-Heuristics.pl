6e9b076432648e2077b51c0898ef897a62533ac2
Marta Portugal <1201845@isep.ipp.pt>
2022-12-13 19:56:37 +0000

    correction heuristic

--- Planning/Site/US4-Heuristics.pl
+++ Planning/Site/US4-Heuristics.pl
@@ -9,7 +9,7 @@ deleteWarehouseVisited([W|RemainingWarehouses],NextWarehouse,MissingWarehouses):
 #retorna o armazem mais proximo. Recebe o armazem de origem, a lista de todos os armazens e o tempo. Vai entao procurar o camiao que tem o mesmo armazem de origem, o de destino e o mesmo tempo.
 #calcula o menor tempo entre os armazens, relativamente ao de origem e o que tiver menor fica o armazem escolhido mais proximo*/
 
-nearestWarehouse(_, [], 0,_):-!.
+nearestWarehouse(_, [], 10000,_):-!.
 nearestWarehouse(Departure,[W1|Warehouses],LessTime,Warehouse):-nearestWarehouse(Departure,Warehouses,LessTime1,Warehouse1), dadosCam_t_e_ta(_,Departure,W1,Time,_,_),
                                                         ((Time<LessTime1,!, LessTime is Time, Warehouse = W1);LessTime is LessTime1, Warehouse = Warehouse1).
 
