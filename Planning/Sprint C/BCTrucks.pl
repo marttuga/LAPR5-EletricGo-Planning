ad7b5d13481c110d28b21f3c0b091cca60202447
Marta Portugal <1201845@isep.ipp.pt>
2023-01-01 21:09:25 +0000

    Sprint C folder

---
+++ Planning/Sprint C/BCTrucks.pl
@@ -0,0 +1,3 @@
+/*factos_camiao
+carateristicasCam(<nome_camiao>,<tara>,<capacidade_carga>,<carga_total_baterias>,<autonomia>,<t_recarr_bat_20a80>).*/
+carateristicasCam(eTruck01,7500,4300,80,100,60).
---
+++ Planning/Sprint C/directory.pl
@@ -0,0 +1,2 @@
+# getWorkingDirectory('C:/Users/pedro/OneDrive/Documentos/Eng.Informatica_Projects/3 Ano/lapr5_g43/projeto base exemplo/ALGAV/').
+getWorkingDirectory('C:/Users/marti/Documents/3ANO1SEMESTRE/LAPR5/lei22_23_s5_3dk_g61_planning/Planning/Sprint B/').
